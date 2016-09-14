a4f7f558653687b1b87bdc154cb16d91c079e6e1
MaDejing <madejing@mioji.com>
2016-09-14 17:10:53 +0800

    视频添加进度条

--- PhotoKitTest/Controller/MyVideoPreviewVC.swift
+++ PhotoKitTest/Controller/MyVideoPreviewVC.swift
@@ -15,10 +15,13 @@ class MyVideoPreviewVC: UIViewController {
 	@IBOutlet weak var m_bottomView: UIView!
 	@IBOutlet weak var m_doneButton: UIButton!
 	@IBOutlet weak var m_playButton: UIButton!
-	
+	@IBOutlet weak var m_slider: UISlider!
+
 	var m_asset: PHAsset!
 	
 	var m_player: AVPlayer!
+	
+	var m_isReadyToPlay = false
 	
 	
     override func viewDidLoad() {
@@ -71,8 +74,12 @@ extension MyVideoPreviewVC {
 				self.view.bringSubviewToFront(self.m_topView)
 				self.view.bringSubviewToFront(self.m_playButton)
 				self.view.bringSubviewToFront(self.m_bottomView)
+				self.view.bringSubviewToFront(self.m_slider)
+				
+				self.updateSliderValue()
 				
 				NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MyVideoPreviewVC.pauseVideo), name: AVPlayerItemDidPlayToEndTimeNotification, object: self.m_player.currentItem)
+				self.m_player.currentItem?.addObserver(self, forKeyPath: "status", options: .New, context: nil)
 			})
 		}
 	}
@@ -86,6 +93,42 @@ extension MyVideoPreviewVC {
 		let image = play ? nil : UIImage(named: "play")
 		self.m_playButton.setImage(image, forState: .Normal)
 	}
+	
+	func updateSliderValue() {
+		// 1秒显示30帧
+		self.m_player.addPeriodicTimeObserverForInterval(CMTimeMake(33, 1000), queue: dispatch_get_main_queue()) {
+			(time) in
+			
+			let current = CMTimeGetSeconds(time)
+			let total = CMTimeGetSeconds(self.m_player.currentItem!.duration)
+			self.m_slider.setValue(Float(current/total), animated: true)
+		}
+	}
+}
+
+extension MyVideoPreviewVC {
+	
+	override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+		if keyPath == "status" {
+			let status: AVPlayerItemStatus = AVPlayerItemStatus(rawValue: (change![NSKeyValueChangeNewKey]?.integerValue)!)!
+			
+			switch status {
+			case .Unknown:
+				self.m_isReadyToPlay = false
+				print("视频资源出现未知错误")
+				break
+			case .ReadyToPlay:
+				self.m_isReadyToPlay = true
+				break
+			case .Failed:
+				self.m_isReadyToPlay = false
+				print("item 有误")
+				break
+			}
+		}
+		
+		object?.removeObserver(self, forKeyPath: "status")
+	}
 }
 
 extension MyVideoPreviewVC {
@@ -94,24 +137,51 @@ extension MyVideoPreviewVC {
 	}
 	
 	@IBAction func playClick(sender: AnyObject) {
-		let curTime = self.m_player.currentItem?.currentTime()
-		let durTime = self.m_player.currentItem?.duration
 		
-		// 0.0代表暂停，1.0代表播放中
-		if (self.m_player.rate == 0.0) {
-			// 播放结束则回到0
-			if (curTime?.value == durTime?.value) {
-				self.m_player.currentItem?.seekToTime(CMTimeMake(0, 1))
+		if (self.m_isReadyToPlay) {
+			let curTime = self.m_player.currentItem?.currentTime()
+			let durTime = self.m_player.currentItem?.duration
+			
+			// 0.0代表暂停，1.0代表播放中
+			if (self.m_player.rate == 0.0) {
+				// 播放结束则回到0
+				if (curTime?.value == durTime?.value) {
+					self.m_player.currentItem?.seekToTime(CMTimeMake(0, 1))
+				}
+				self.playVideo()
+			} else {
+				self.pauseVideo()
 			}
-			self.m_player.play()
-			self.updateTopBottomView(true)
-			self.updatePlayButton(true)
 		} else {
-			self.pauseVideo()
+			print("视频加载中。。。")
 		}
 	}
 	
 	@IBAction func doneClick(sender: AnyObject) {
+	}
+	
+	@IBAction func sliderAction(sender: AnyObject) {
+		let durTime = Float(CMTimeGetSeconds((self.m_player.currentItem?.duration)!))
+
+		let startTime = CMTimeMake(Int64(self.m_slider.value*durTime), 1)
+		
+		self.m_player.currentItem?.seekToTime(startTime, completionHandler: { (finished) in
+			if (finished) {
+				if (self.m_isReadyToPlay) {
+					if (self.m_player.rate == 0.0) {
+						self.pauseVideo()
+					} else {
+						self.playVideo()
+					}
+				}
+			}
+		})
+	}
+	
+	func playVideo() {
+		self.m_player.play()
+		self.updateTopBottomView(true)
+		self.updatePlayButton(true)
 	}
 	
 	func pauseVideo() {
